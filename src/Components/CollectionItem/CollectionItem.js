cc0cf700f4e5e40e575688906d10ed3d2f9d9f0d
ankur-b <barveankur29@outlook.com>
2021-08-22 09:52:15 +0530

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/Components/CollectionItem/CollectionItem.js
+++ src/Components/CollectionItem/CollectionItem.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React,{useContext} from "react";
+import CustomButton from '../CustomButton/CustomButton';
 import "./CollectionItem.css";
-
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+import {Context as CartContext} from '../../Context/CartContext';
+const CollectionItem = ({ item }) => {
+  const {name, price, imageUrl} = item
+  const {state,addItem} = useContext(CartContext);
   return (
     <div className="collection-item">
+      {console.log(state.cartItems)}
       <div
         className="image"
         style={{
@@ -14,6 +18,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton onClick={()=>addItem(item)}>Add to Cart</CustomButton>
     </div>
   );
 };
--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,8 +1,14 @@
 import CreateDataContext from './CreateDataContext'; 
+import {addItemToCart} from './CartUtils'
 const CartReducer = (state, action) => {
   switch (action.type) {
     case 'TOGGLE_CART_HIDDEN':
       return {...state,hidden:!state.hidden};
+    case 'ADD_ITEM':
+      return {
+        ...state,
+        cartItems:addItemToCart(state.cartItems,action.payload)
+      }
     default:
       return state;
   }
@@ -11,8 +17,11 @@ const CartReducer = (state, action) => {
 const toggleCartHidden = dispatch => ()=>{
     dispatch({type:'TOGGLE_CART_HIDDEN'})
 }
+const addItem = dispatch => (item) =>{
+  dispatch({type:'ADD_ITEM',payload:item})
+}
 export const {Provider, Context} = CreateDataContext(
   CartReducer,
-  {toggleCartHidden},
-  {hidden: true},
+  {toggleCartHidden,addItem},
+  {hidden: true,cartItems:[]},
 );
