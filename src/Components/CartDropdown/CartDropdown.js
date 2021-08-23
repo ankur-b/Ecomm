91e4548b89ec3132614213e72ce7f357f5f0ca47
ankur-b <barveankur29@outlook.com>
2021-08-23 08:59:34 +0530

    created cart-item component and connected our cart dropdown with cartcontext

--- src/Components/CartDropdown/CartDropdown.js
+++ src/Components/CartDropdown/CartDropdown.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import CustomButton from '../CustomButton/CustomButton';
+import CartItem from '../CartItem/CartItem';
+
+import {Context as CartContext} from '../../Context/CartContext';
+
 import './CartDropdown.css';
 
 const CartDropdown = ()=>{
+    const {state} = useContext(CartContext);
     return(
         <div className="cart-dropdown">
-            <div className="cart-items"/>
+            <div className="cart-items">
+                {
+                    state.cartItems.map(cartItem=><CartItem key={cartItem.id} item={cartItem}/>)
+                }
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
