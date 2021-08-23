741db9df35519db96a682ea79dbefd0d668c956d
ankur-b <barveankur29@outlook.com>
2021-08-23 19:55:55 +0530

    added addItem and removeItem functionality to checkout item component

--- src/Components/CheckoutItem/CheckoutItem.js
+++ src/Components/CheckoutItem/CheckoutItem.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React,{useContext} from "react";
+import {Context as CartContext} from "../../Context/CartContext";
 import "./CheckoutItem.css";
 
-const CheckoutItem = ({cartItem:{name,imageUrl,price,quantity}}) => {
+const CheckoutItem = ({cartItem}) => {
+  const {clearItemFromCart,addItem,removeItem} = useContext(CartContext)
+  const {name,imageUrl,price,quantity} = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={()=>removeItem(cartItem)}>&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={()=>addItem(cartItem)}>&#10095;</div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={()=>clearItemFromCart(cartItem)}>&#10005;</div>
     </div>
   );
 };
--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,27 +1,46 @@
-import CreateDataContext from './CreateDataContext'; 
-import {addItemToCart} from './CartUtils'
+import CreateDataContext from "./CreateDataContext";
+import { addItemToCart,removeItemFromCart } from "./CartUtils";
 const CartReducer = (state, action) => {
   switch (action.type) {
-    case 'TOGGLE_CART_HIDDEN':
-      return {...state,hidden:!state.hidden};
-    case 'ADD_ITEM':
+    case "TOGGLE_CART_HIDDEN":
+      return { ...state, hidden: !state.hidden };
+    case "ADD_ITEM":
       return {
         ...state,
-        cartItems:addItemToCart(state.cartItems,action.payload)
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case "CLEAR_ITEM_FROM_CART":
+      return {
+        ...state,
+        cartItems:state.cartItems.filter(cartItem=>cartItem.id!==action.payload.id)
+      }
+    case "REMOVE_ITEM":
+      return {
+        ...state,
+        cartItems:removeItemFromCart(state.cartItems,action.payload)
       }
     default:
       return state;
   }
 };
 
-const toggleCartHidden = dispatch => ()=>{
-    dispatch({type:'TOGGLE_CART_HIDDEN'})
+const toggleCartHidden = (dispatch) => () => {
+  dispatch({ type: "TOGGLE_CART_HIDDEN" });
+};
+const addItem = (dispatch) => (item) => {
+  dispatch({ type: "ADD_ITEM", payload: item });
+};
+const clearItemFromCart = (dispatch) => (item) => {
+  dispatch({type:"CLEAR_ITEM_FROM_CART",payload:item})
+};
+const removeItem = (dispatch) =>(item)=>{
+  dispatch({type:"REMOVE_ITEM",payload:item})
 }
-const addItem = dispatch => (item) =>{
-  dispatch({type:'ADD_ITEM',payload:item})
-}
-export const {Provider, Context} = CreateDataContext(
+// const addItem = (dispatch) =>(item)=>{
+//   dispatch({})
+// }
+export const { Provider, Context } = CreateDataContext(
   CartReducer,
-  {toggleCartHidden,addItem},
-  {hidden: true,cartItems:[]},
+  { toggleCartHidden, addItem, clearItemFromCart ,removeItem},
+  { hidden: true, cartItems: [] }
 );
