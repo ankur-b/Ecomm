2578664061f3c8e285494612a8fd2d9243a78efb
ankur-b <ankurbarve29@gmail.com>
2021-10-16 13:49:51 +0530

    stripe payment gateway added

--- src/Components/StripeButton/StripeButton.js
+++ src/Components/StripeButton/StripeButton.js
@@ -1,10 +1,23 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios';
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_qwduYzRdaR1aFqSKz5dMMUE500BFmJcXjq";
   const onToken = (token) => {
-    
+    axios({
+      url:"payment",
+      method:"post",
+      data:{
+        amount:priceForStripe,
+        token:token
+      }
+    }).then(response=>{
+      alert("Payment Successful")
+    }).catch(error=>{
+      console.log("Payment error:",JSON.parse(error));
+      alert("There was an issue with your payment.Please sure you use the provided credit card.")
+    })
   };
   return (
       <StripeCheckout
